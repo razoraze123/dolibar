2c77f5d5ba7d6b390217c8f5f89aa5572feb608c
razoraze123 <garbalamine@yahoo.com>
2025-07-09 23:38:40 +0200

    Update modEcommerce.class.php

--- core/modules/modEcommerce.class.php
+++ core/modules/modEcommerce.class.php
@@ -325,7 +325,7 @@ class modEcommerce extends DolibarrModules
 			'prefix' => img_picto('', $this->picto, 'class="pictofixedwidth valignmiddle"'),
 			'mainmenu' => 'ecommerce',
 			'leftmenu' => '',
-			'url' => '/ecommerce/ecommerceindex.php',
+			'url' => '/custom/ecommerce/ecommerceindex.php',
 			'langs' => 'ecommerce@ecommerce', // Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
 			'position' => 1000 + $r,
                         'enabled' => "isModEnabled('ecommerce')", // Define condition to show or hide menu entry. Use 'isModEnabled('ecommerce')' if entry must be visible if module is enabled.
